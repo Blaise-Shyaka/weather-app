dafe8fbaeefb86aa783fd373dec82df7bac0c60d
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-03-05 23:13:10 +0200

    style the weather app main page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 const apiKey = 'c15a8021aaa6d4c7b6fb2a77982f0396';
 
 const displayData = (data) => {
@@ -6,6 +8,10 @@ const displayData = (data) => {
   const windspeedTag = document.querySelector('#windspeed-tag');
   const temperatureTag = document.querySelector('#temperature-tag');
   const errorTag = document.querySelector('#error-tag');
+  const cityTag = document.querySelector('#city-tag');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const unitSign = document.querySelector('#unit');
+
   errorTag.innerHTML = '';
 
   if (data.cod === '400') {
@@ -19,15 +25,19 @@ const displayData = (data) => {
   }
 
   weatherTag.innerHTML = data.weather[0].description;
-  temperatureTag.innerHTML = data.main.temp - 273.15;
-  visibilityTag.innerHTML = data.visibility;
-  windspeedTag.innerHTML = data.wind.speed;
+  temperatureTag.innerHTML = (data.main.temp - 273.15).toFixed(1);
+  visibilityTag.innerHTML = `Visibility: ${data.visibility}`;
+  windspeedTag.innerHTML = `Wind Speed: ${data.wind.speed}`;
+  cityTag.innerHTML = data.name;
+  unitSign.innerHTML = '<sup>o</sup>C';
+  weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 };
 
 const getWeatherData = async (cityName) => {
   try {
     const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`);
     const response = await result.json();
+    console.log(response);
     return displayData(response);
   } catch (e) {
     return e;
